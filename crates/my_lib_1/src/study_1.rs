14676b7e13103ec0a98f098957158f85f5014403
yb <scauyb@qq.com>
2025-10-04 08:23:40 +0800

    test(study_1): 添加集成测试模块- 为 study_1 模块添加测试模块 - 引入 trait_2进行 的 main 函数测试 - 配置测试环境并运行基本测试用例
    Signed-off-by: yb <scauyb@qq.com>

--- crates/my_lib_1/src/study_1.rs
+++ crates/my_lib_1/src/study_1.rs
@@ -2,3 +2,13 @@ pub mod fn_methods;
 pub mod print_1;
 pub mod trait_1;
 pub mod trait_2;
+
+#[cfg(test)]
+mod tests {
+    use crate::study_1::*;
+
+    #[test]
+    fn it_works() {
+        trait_2::main()
+    }
+}
