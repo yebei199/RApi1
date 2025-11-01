070405874586464d0ef5799093ce35424399bcea
yb <scauyb@qq.com>
2025-11-01 08:33:59 +0800

    feat(bevy): 添加实体位置组件和打印系统
    - 添加 Position 组件定义- 实现 print_position_system 系统- 添加 Entity 结构体
    - 实现 hello_world 系统
    - 在 App 中注册 hello_world 系统到 Update 阶段

--- crates/bevy_1/src/main.rs
+++ crates/bevy_1/src/main.rs
@@ -1,5 +1,19 @@
 use bevy::prelude::*;
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+fn print_position_system(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {} {}", position.x, position.y);
+    }
+}
+struct Entity(u64);
+fn hello_world() {
+    println!("hello world!");
+}
 
 fn main() {
-    App::new().run();
+    App::new().add_systems(Update, hello_world).run();
 }
